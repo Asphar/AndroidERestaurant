4f72ac9a9c6b6df0ac33b8107eb015184723097e
Asphar <davidnguyen7500@gmail.com>
2022-01-25 16:22:31 +0100

    Show toast message

--- app/src/main/java/fr/isen/david/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/david/androiderestaurant/HomeActivity.kt
@@ -2,10 +2,31 @@ package fr.isen.david.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
+import fr.isen.david.androiderestaurant.databinding.ActivityHomeBinding
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        // setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener(){
+            Toast.makeText(this,"Choose your appetizer",Toast.LENGTH_SHORT).show()
+        }
+
+        binding.button2.setOnClickListener(){
+            Toast.makeText(this,"Choose your main course",Toast.LENGTH_SHORT).show()
+        }
+
+        binding.button3.setOnClickListener(){
+            Toast.makeText(this,"Choose your dessert",Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
