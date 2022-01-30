a114f0bdcb5e63c694aa6f848422176d82e657ef
Asphar <davidnguyen7500@gmail.com>
2022-01-30 23:49:46 +0100

    Passing textView argument

--- app/src/main/java/fr/isen/david/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/david/androiderestaurant/HomeActivity.kt
@@ -1,10 +1,10 @@
 package fr.isen.david.androiderestaurant
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import fr.isen.david.androiderestaurant.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -15,23 +15,40 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
         binding.button.setOnClickListener(){
             val intent = Intent(this, MenuActivity::class.java)
             Toast.makeText(this,"Choose your appetizer",Toast.LENGTH_SHORT).show()
+            intent.putExtra("Button Text", "Appetizer menu")
             startActivity(intent)
+
         }
 
         binding.button2.setOnClickListener(){
             val intent = Intent(this, MenuActivity::class.java)
             Toast.makeText(this,"Choose your main course",Toast.LENGTH_SHORT).show()
+            intent.putExtra("Button Text", "Main course menu")
             startActivity(intent)
         }
 
         binding.button3.setOnClickListener(){
             val intent = Intent(this, MenuActivity::class.java)
             Toast.makeText(this,"Choose your dessert",Toast.LENGTH_SHORT).show()
+            intent.putExtra("Button Text", "Dessert menu")
             startActivity(intent)
         }
-        
+
     }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("HomeActivity", "HomeActivity has been stopped");
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("HomeActivity", "HomeActivity has been destroyed");
+    }
+
+
 }
--- app/src/main/java/fr/isen/david/androiderestaurant/MenuActivity.kt
+++ app/src/main/java/fr/isen/david/androiderestaurant/MenuActivity.kt
@@ -1,11 +1,25 @@
 package fr.isen.david.androiderestaurant
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent.getIntent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MenuActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        val ss:String = intent.getStringExtra("Button Text").toString()
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.setText(ss).toString()
+        print(textView)
+        val textViewValue = textView.text
+
     }
+
 }
+
+
