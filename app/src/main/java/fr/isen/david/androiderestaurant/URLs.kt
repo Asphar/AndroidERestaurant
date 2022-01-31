7f8653963318a417999fe6656a79e25173abaa7c
Asphar <davidnguyen7500@gmail.com>
2022-01-31 16:10:41 +0100

    Removing url object

--- app/src/main/java/fr/isen/david/androiderestaurant/URLs.kt
+++
@@ -1,7 +0,0 @@
-package fr.isen.david.androiderestaurant
-
-object URLs {
-    private val ROOT_URL = "http://test.api.catering.bluecodegames.com/menu"
-    val URL_REGISTER = ROOT_URL + "id_shop"
-
-}
